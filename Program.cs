69338a92fdcfc7a76172232148cfb868d46b9ce1
PBL075 <pavelgorea75@gmail.com>
2026-04-22 20:19:55 +0300

    Primul meu proiect CSharp cu Nissan GTR

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GarajPavel
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Variabilele noastre
+            string masinaMea = "Nissan GT-R R35";
+            int caiPutere = 570;
+            double acceleratie = 2.8;
+            int pretEuro = 120000;
+            string culoarea = "Neagra";
+
+            // Afisarea informatiilor
+            Console.WriteLine("=== SISTEM GESTIUNE GARAJ ===");
+            Console.WriteLine("Masina curenta: " + masinaMea);
+            Console.WriteLine("Putere: " + caiPutere + " HP");
+            Console.WriteLine("Viteza 0-100 km/h: " + acceleratie + " sec");
+            Console.WriteLine("Pret estimativ: " + pretEuro + " EUR");
+            Console.WriteLine("Culoarea: " + culoarea );
+            Console.WriteLine("============================");
+            
+            Console.WriteLine("\nStatus: Masina este gata de drum");
+        }
+    }
+}
